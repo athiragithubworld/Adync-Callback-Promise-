29d8739b3a96b20aeeaf794e5cf9f4a7b033db1f
athiragithubworld <athirahrms@gmail.com>
2022-12-24 16:19:49 +0530

    promise_Async_Await(Using AsyncAwait with promiseall - Eg:Premovie tickets - add colddrinks)

--- promise_Async_Await.js
+++ promise_Async_Await.js
@@ -37,13 +37,64 @@
 // console.log('person5:shows ticket');
 
 
-// // Coding using async await-------------------------------------------
+// // // Coding using async await-------------------------------------------
+// console.log('person1:shows ticket');
+// console.log('person2:shows ticket');
+
+// // // Async function expression, we are calling inside the constant variable
+// // const preMovie =async ()=> 'preMovie';
+// // preMovie().then((m) => console.log(m));
+
+// const preMovie =async ()=> {
+  
+//     const promiseWifeBringsTickets = new Promise((resolve, reject) => {
+//     setTimeout(() => resolve('tickets'), 3000);
+// });
+ 
+// const getPopcorn = new Promise((resolve,reject)=>resolve(`popcorn`));
+
+// const getButter = new Promise((resolve,reject) => resolve(`butter`));
+
+// const getColdDrinks =new Promise((resolve,reject) => resolve(`ColdDrinks`));
+
+// let ticket = await promiseWifeBringsTickets;
+
+// console.log(`wife:I have the ${ticket}` );
+// console.log('husband :we should go in');
+// console.log('wife:No I am hungry');
+
+// let popcorn = await getPopcorn;
+
+// console.log(`husband : I got some ${popcorn}`);
+// console.log('husband : we should go in');
+// console.log('wife:I need butter on my popcorn');
+
+// let butter = await getButter;
+
+// console.log(`husband : I got some ${butter}`);
+// console.log('husband : we should go in');
+// console.log('wife:I need some cold drinks  ');
+
+// let coldDrinks = await getColdDrinks;
+
+// console.log(`husband : I got some ${coldDrinks}`);
+// console.log(`husband: anything else darling`);
+// console.log(`wife : lets go we are getting late`);
+// console.log(`husband: thank you for the reminder *grins*`);
+
+// return ticket;
+
+// }
+
+// preMovie().then((m) => console.log(`person3 : shows ${m}`));
+
+// console.log('person4:shows ticket');
+// console.log('person5:shows ticket');
+
+// //--------------using promise all in Async await & Using error handling-------------// //
+
 console.log('person1:shows ticket');
 console.log('person2:shows ticket');
-
-// // Async function expression, we are calling inside the constant variable
-// const preMovie =async ()=> 'preMovie';
-// preMovie().then((m) => console.log(m));
 
 const preMovie =async ()=> {
   
@@ -51,36 +102,32 @@ const preMovie =async ()=> {
     setTimeout(() => resolve('tickets'), 3000);
 });
  
+//-------- using promise all in Async await ------------------////
+
 const getPopcorn = new Promise((resolve,reject)=>resolve(`popcorn`));
-
 const getButter = new Promise((resolve,reject) => resolve(`butter`));
-
 const getColdDrinks =new Promise((resolve,reject) => resolve(`ColdDrinks`));
 
 let ticket = await promiseWifeBringsTickets;
 
-console.log(`wife:I have the ${ticket}` );
-console.log('husband :we should go in');
-console.log('wife:No I am hungry');
+let [Popcorn, Butter ,Colddrinks] = await Promise.all([getPopcorn ,getButter , getColdDrinks]);
+console.log(`${Popcorn} ,${Butter} , ${Colddrinks}`)
 
-let popcorn = await getPopcorn;
 
-console.log(`husband : I got some ${popcorn}`);
-console.log('husband : we should go in');
-console.log('wife:I need butter on my popcorn');
+// // // ------Using error handling------------------//
 
-let butter = await getButter;
+// const preMovie =async ()=> {
+  
+//     const promiseWifeBringsTickets = new Promise((resolve, reject) => {
+//     setTimeout(() => reject('tickets'), 3000);
+// });
 
-console.log(`husband : I got some ${butter}`);
-console.log('husband : we should go in');
-console.log('wife:I need some cold drinks  ');
-
-let coldDrinks = await getColdDrinks;
-
-console.log(`husband : I got some ${coldDrinks}`);
-console.log(`husband: anything else darling`);
-console.log(`wife : lets go we are getting late`);
-console.log(`husband: thank you for the reminder *grins*`);
+// let ticket
+// try {
+//     ticket=await promiseWifeBringsTickets;
+// } catch (e) {
+//     ticket = 'sad face';
+// }
 
 return ticket;
 
@@ -90,3 +137,4 @@ preMovie().then((m) => console.log(`person3 : shows ${m}`));
 
 console.log('person4:shows ticket');
 console.log('person5:shows ticket');
+
